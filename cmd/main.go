1860cab26bfeb57f793cec3f8352ca98068ee69c
Raphael Berger <raphaelmb@gmail.com>
2024-01-05 11:10:10 -0300

    setting up

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Renderer = NewTemplates()
+
+	count := Count{Count: 0}
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", count)
+	})
+
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(http.StatusOK, "index", count)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
