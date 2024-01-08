41377184b6c8f33394cfc5acde2832775315d3b9
Raphael Berger <raphaelmb@gmail.com>
2024-01-08 16:50:03 -0300

    add form + email validation

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
@@ -23,24 +24,97 @@ func NewTemplates() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func NewContact(name, email string) *Contact {
+	return &Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts []*Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func NewData() *Data {
+	return &Data{
+		Contacts: Contacts{
+			NewContact("John", "jd@gmail.com"),
+			NewContact("Clara", "cd@gmail.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func NewFormData() *FormData {
+	return &FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func NewPage() *Page {
+	return &Page{
+		Data: *NewData(),
+		Form: *NewFormData(),
+	}
 }
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
+	page := NewPage()
 	e.Renderer = NewTemplates()
 
-	count := Count{Count: 0}
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "index", count)
+		return c.Render(http.StatusOK, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(http.StatusOK, "index", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := NewFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			fmt.Printf("%+v\n", formData)
+
+			return c.Render(http.StatusUnprocessableEntity, "form", formData)
+		}
+
+		contact := NewContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		c.Render(http.StatusOK, "form", NewFormData())
+		return c.Render(http.StatusCreated, "oob-contact", contact)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
